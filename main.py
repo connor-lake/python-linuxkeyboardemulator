4375aa2497062042a6057e91f37d1658c26fcc45
ifr68jtu92 <ifr68jtu92@mailvad.com>
2026-06-07 11:23:19 +0100

    add script

---
+++ main.py
@@ -0,0 +1,33 @@
+from evdev import ecodes, InputDevice
+import evdev
+from time import sleep
+devices = [InputDevice(_) for _ in evdev.list_devices()]
+for _ in devices:
+    print(_.path, _.name, _.phys)
+print("If you are unable to see your device, try running as root.")
+d = input("Event Path (e.g. /dev/input/event0): ")
+k = input("Key (e.g: SPACE, A, ENTER): ").upper()
+delay = int(input("Delay in ms (1000 - 1s, 3500 - 3.5s): "))
+
+
+device = InputDevice(d)
+key = getattr(ecodes, f'KEY_{k}')
+
+while True:
+    try:
+         # device.write queues keypresses to the device,
+         # device.syn synchroinises keypresse and fires
+         # all events that are in queue
+         sleep(delay / 1000)
+         for _ in range(2):
+             device.write(ecodes.EV_KEY, key, _-1)
+             device.syn()
+             sleep(0.1)
+         # for some reason, some applications will bug
+         # if i do not .syn() after down and up
+         # they also need a delay, for some reason
+         # tested: injustice 2 on wine
+         print(f"KeyPress KEY_{k} simulated, CTRL+C to exit.")
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt, exiting...")
+        exit()
